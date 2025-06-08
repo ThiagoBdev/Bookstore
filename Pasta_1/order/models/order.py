a351d99d49e3ad67e8676e9f4ea63599bec50af4
thiago batista <thiagobatista15390@gmail.com>
2025-06-08 20:30:00 -0300

    Remove submódulo antigo Pasta_1 e adiciona como pasta comum

---
+++ Pasta_1/order/models/order.py
@@ -0,0 +1,11 @@
+
+from django.contrib.auth.models import User
+from Bookstore.product.models.product import Product
+
+
+
+from django.db import models
+
+class Order(models.Model):
+    product = models.ManyToManyField(Product, blank=False)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
